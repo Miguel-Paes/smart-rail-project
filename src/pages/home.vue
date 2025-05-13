cfa8e9309b9e27d0c08434fd41089be307ee4ea9
Miguel-Paes <miguelpaesdasilva@gmail.com>
2025-05-13 11:50:17 -0300

    card de avisos

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -51,131 +51,146 @@
   <v-main class="pa-0 pt-1" :class="displayStore.xs ? 'px-3 py-1' : displayStore.sm ? 'px-5 py-2' : 'px-7 py-3'">
     <h2 class="mb-3 font-weight-bold" :class="displayStore.xs ? 'text-h6' : displayStore.sm ? 'text-h5' : 'text-h4'">Dashboard Geral</h2>
 
-    <v-row class="d-flex" :class="displayStore.xs ? '' : displayStore.sm ? '' : 'ga-3'" dense>
-      <v-col class="d-flex align-center pa-0" cols="12" md="3" sm="6">
-        <v-select
-          v-model="stationStore.stationId"
-          item-title="name"
-          item-value="id"
-          :items="stationStore.stations"
-          label="Escolha a estação"
-          variant="underlined"
-          @update:model-value="stationStore.getStationLines"
-        />
-      </v-col>
+    <v-row class="d-flex">
+      <v-col class="d-flex flex-column justify-center" cols="12" md="6">
+        <v-row class="d-flex" :class="displayStore.xs ? '' : displayStore.sm ? '' : 'ga-3'" dense>
+          <v-col class="d-flex align-center pa-0" cols="12" sm="6">
+            <v-select
+              v-model="stationStore.stationId"
+              item-title="name"
+              item-value="id"
+              :items="stationStore.stations"
+              label="Escolha a estação"
+              variant="underlined"
+              @update:model-value="stationStore.getStationLines"
+            />
+          </v-col>
 
-      <v-col
-        class="d-flex align-center pa-0"
-        :class="displayStore.xs ? 'justify-center' : displayStore.sm ? 'justify-center' : 'justify-start'"
-        cols="12"
-        md="3"
-        sm="6"
-      >
-        <v-dialog>
-          <template #activator="{ props: activatorProps }">
-            <v-btn
-              class="pa-2 d-flex align-center h-75 text-no-wrap"
-              :class="$vuetify.display.mobile ? 'w-100 h-25' : 'h-50 w-100' "
-              color="red-accent-3"
-              v-bind="activatorProps"
-              variant="tonal"
-            >
-              Não sei a estação mais próxima
-            </v-btn>
-          </template>
+          <v-col
+            class="d-flex align-center pa-0"
+            :class="displayStore.xs ? 'justify-center' : displayStore.sm ? 'justify-center' : 'justify-start'"
+            cols="12"
+            sm="5"
+          >
+            <v-dialog>
+              <template #activator="{ props: activatorProps }">
+                <v-btn
+                  class="pa-2 d-flex align-center h-75 text-no-wrap"
+                  :class="$vuetify.display.mobile ? 'w-100 h-25' : 'h-50 w-100' "
+                  color="red-accent-3"
+                  v-bind="activatorProps"
+                  variant="tonal"
+                >
+                  Não sei a estação mais próxima
+                </v-btn>
+              </template>
 
-          <template #default="{ isActive }">
-            <v-card :class="$vuetify.display.mobile ? 'pa-1' : 'pa-7'" title="Digite as informações">
-              <v-form>
-                <v-row class="d-flex">
-                  <v-col cols="12" lg="6">
-                    <v-text-field
-                      v-model="cep"
-                      dense
-                      label="Digite o CEP desejado..."
-                      maxlength="8"
-                      @input="onCepInput"
-                    />
-                  </v-col>
-
-                  <v-col cols="12" lg="6">
-                    <v-text-field
-                      v-model="stationStore.userLocation.departureTime"
-                      :active="menu2"
-                      :focus="menu2"
-                      label="Horário de Saída"
-                      prepend-icon="mdi-clock-time-four-outline"
-                      readonly
-                    >
-                      <v-menu
-                        v-model="menu2"
-                        activator="parent"
-                        :close-on-content-click="false"
-                        transition="scale-transition"
-                      >
-                        <v-time-picker
-                          v-if="menu2"
-                          v-model="stationStore.userLocation.departureTime"
-                          ampm-in-title="true"
-                          full-width
+              <template #default="{ isActive }">
+                <v-card :class="$vuetify.display.mobile ? 'pa-1' : 'pa-7'" title="Digite as informações">
+                  <v-form>
+                    <v-row class="d-flex">
+                      <v-col cols="12" lg="6">
+                        <v-text-field
+                          v-model="cep"
+                          dense
+                          label="Digite o CEP desejado..."
+                          maxlength="8"
+                          @input="onCepInput"
                         />
-                      </v-menu>
-                    </v-text-field>
-                  </v-col>
-                </v-row>
+                      </v-col>
 
-                <v-row class="d-flex">
-                  <v-col cols="12" lg="6">
-                    <v-select v-model="stationStore.userLocation.state" :items="states" label="Estado" variant="outlined" />
-                  </v-col>
+                      <v-col cols="12" lg="6">
+                        <v-text-field
+                          v-model="stationStore.userLocation.departureTime"
+                          :active="menu2"
+                          :focus="menu2"
+                          label="Horário de Saída"
+                          prepend-icon="mdi-clock-time-four-outline"
+                          readonly
+                        >
+                          <v-menu
+                            v-model="menu2"
+                            activator="parent"
+                            :close-on-content-click="false"
+                            transition="scale-transition"
+                          >
+                            <v-time-picker
+                              v-if="menu2"
+                              v-model="stationStore.userLocation.departureTime"
+                              ampm-in-title="true"
+                              full-width
+                            />
+                          </v-menu>
+                        </v-text-field>
+                      </v-col>
+                    </v-row>
 
-                  <v-col cols="12" lg="6">
-                    <v-text-field v-model="stationStore.userLocation.city" label="Cidade" variant="outlined" />
-                  </v-col>
-                </v-row>
+                    <v-row class="d-flex">
+                      <v-col cols="12" lg="6">
+                        <v-select v-model="stationStore.userLocation.state" :items="states" label="Estado" variant="outlined" />
+                      </v-col>
 
-                <v-row class="d-flex">
-                  <v-col cols="12" lg="6">
-                    <v-text-field v-model="stationStore.userLocation.street" label="Rua" variant="outlined" />
-                  </v-col>
+                      <v-col cols="12" lg="6">
+                        <v-text-field v-model="stationStore.userLocation.city" label="Cidade" variant="outlined" />
+                      </v-col>
+                    </v-row>
 
-                  <v-col cols="12" lg="6">
-                    <v-text-field v-model="stationStore.userLocation.number" label="Número" variant="outlined" />
-                  </v-col>
-                </v-row>
+                    <v-row class="d-flex">
+                      <v-col cols="12" lg="6">
+                        <v-text-field v-model="stationStore.userLocation.street" label="Rua" variant="outlined" />
+                      </v-col>
 
-                <v-row class="d-flex justify-center">
-                  <v-btn color="red-accent-3" variant="tonal" @click="isActive.value = false">
-                    Descobrir estação
-                  </v-btn>
-                </v-row>
-              </v-form>
-            </v-card>
-          </template>
-        </v-dialog>
+                      <v-col cols="12" lg="6">
+                        <v-text-field v-model="stationStore.userLocation.number" label="Número" variant="outlined" />
+                      </v-col>
+                    </v-row>
+
+                    <v-row class="d-flex justify-center">
+                      <v-btn color="red-accent-3" variant="tonal" @click="isActive.value = false">
+                        Descobrir estação
+                      </v-btn>
+                    </v-row>
+                  </v-form>
+                </v-card>
+              </template>
+            </v-dialog>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-table
+              fixed-header
+            >
+              <thead>
+                <tr>
+                  <th class="text-left">Nome</th>
+                  <th class="text-left">Parada</th>
+                  <th class="text-left">Destino Final</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="line in stationStore.stationLines" :key="line.id">
+                  <td>{{ line.name }}</td>
+                  <td>{{ line.stop }}</td>
+                  <td>{{ line.finalDestination }}</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-col>
+        </v-row>
       </v-col>
-    </v-row>
-    <v-row>
+
       <v-col cols="12" md="6">
-        <v-table
-          fixed-header
-        >
-          <thead>
-            <tr>
-              <th class="text-left">Nome</th>
-              <th class="text-left">Parada</th>
-              <th class="text-left">Destino Final</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="line in stationStore.stationLines" :key="line.id">
-              <td>{{ line.name }}</td>
-              <td>{{ line.stop }}</td>
-              <td>{{ line.finalDestination }}</td>
-            </tr>
-          </tbody>
-        </v-table>
+        <v-card class="w-100 h-100 pa-2 border-red-accent-3 rounded rounded-lg" color="red-accent-3" variant="tonal">
+          <v-card-title class="text-center">AVISOS</v-card-title>
+        </v-card>
       </v-col>
     </v-row>
   </v-main>
 </template>
+
+<style>
+  .border-red-accent-3 {
+  border: solid .1rem #FF1744;
+}
+</style>
