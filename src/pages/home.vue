da73e5a3c32bb2e96e9a3830048b71eee3b85794
Miguel-Paes <miguelpaesdasilva@gmail.com>
2025-05-29 12:56:01 -0300

    cores adicionadas na página home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -52,8 +52,8 @@
   </script>
 
 <template>
-  <v-main class="pa-0 pt-1" :class="displayStore.xs ? 'px-3 py-1' : displayStore.sm ? 'px-5 py-2' : 'px-7 py-3'">
-    <h2 class="mb-3 font-weight-bold" :class="displayStore.xs ? 'text-h6' : displayStore.sm ? 'text-h5' : 'text-h4'">Dashboard Geral</h2>
+  <v-main class="pa-0 pt-5" :class="displayStore.xs ? 'px-3 py-1' : displayStore.sm ? 'px-5 py-2' : 'px-7 py-3'">
+    <h1 class="mb-3 font-weight-regular" :class="displayStore.xs ? 'text-h5' : displayStore.sm ? 'text-h4' : 'text-h3'">Dashboard Geral</h1>
 
     <v-row class="d-flex">
       <v-col class="d-flex flex-column justify-center" cols="12" md="6">
