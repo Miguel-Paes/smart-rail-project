ff0f469004030f0ffd9b6ce6c7c5f05acc08e93e
Miguel-Paes <miguelpaesdasilva@gmail.com>
2025-06-05 10:51:21 -0300

    cabeçalho da página de rotas

--- src/pages/routes.vue
+++ src/pages/routes.vue
@@ -1,5 +1,12 @@
+<script setup>
+  import { useDisplayStore } from '@/stores/displayStore.js';
+
+  const displayStore = useDisplayStore();
+</script>
+
 <template>
-  <v-main class="pa-0 pt-1" :class="$vuetify.display.mobile ? 'px-3' : 'px-7'">
-    <h2 class="mb-3" :class="$vuetify.display.mobile ? '' : 'text-h4 font-weight-bold'">Gestão de Rotas</h2>
+  <v-main class="pa-0 pt-5" :class="displayStore.xs ? 'px-3 py-1' : displayStore.sm ? 'px-5 py-2' : 'px-7 py-3'">
+    <h1 class="mb-3 font-weight-regular" :class="displayStore.xs ? 'text-h5' : displayStore.sm ? 'text-h4' : 'text-h3'">Gestão de Rotas</h1>
+
   </v-main>
 </template>
