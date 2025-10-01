0739d1e62c09cb4f30b98fb911a798270a9fb154
miguel-paes <miguelpaesdasilva@gmail.com>
2025-10-01 12:53:20 -0300

    add: textura de fundo para todas as páginas

--- src/layouts/fullPage.vue
+++ src/layouts/fullPage.vue
@@ -1,15 +1,14 @@
 <script setup>
-  import { RouterView } from 'vue-router'
-  import { useDisplayStore } from '@/stores/displayStore.js'
+import { RouterView } from "vue-router";
+import { useDisplayStore } from "@/stores/displayStore.js";
 
-  import HeaderComponent from '@/components/HeaderComponent.vue'
+import HeaderComponent from "@/components/HeaderComponent.vue";
 
-  const displayStore = useDisplayStore()
-
+const displayStore = useDisplayStore();
 </script>
 
 <template>
-  <v-app>
+  <v-app class="background-texture">
     <HeaderComponent />
     <v-main>
       <section class="pa-4">
@@ -18,3 +17,12 @@
     </v-main>
   </v-app>
 </template>
+
+<style scoped>
+.background-texture {
+  background-image: url("/images/fullPage-bg.png");
+  background-repeat: repeat;
+  background-size: contain;
+  min-height: 100vh;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,53 +1,65 @@
-  <script setup>
-  import { useStationStore } from '@/stores/stationStore.js'
-  import { useDisplayStore } from '@/stores/displayStore.js'
-  import { useWarningStore } from '@/stores/warningsStore';
-  
-  import FindStationDialog from '@/components/home/FindStationDialog.vue';
-  import WarningsList from '@/components/template/WarningsList.vue';
+<script setup>
+import { useStationStore } from "@/stores/stationStore.js";
+import { useDisplayStore } from "@/stores/displayStore.js";
+import { useWarningStore } from "@/stores/warningsStore";
 
-  const stationStore = useStationStore()
-  const displayStore = useDisplayStore()
-  const warningStore = useWarningStore()
+import FindStationDialog from "@/components/home/FindStationDialog.vue";
+import WarningsList from "@/components/template/WarningsList.vue";
+
+const stationStore = useStationStore();
+const displayStore = useDisplayStore();
+const warningStore = useWarningStore();
 </script>
 
 <template>
-    <TitleSection title="Dashboard Geral" />
-    <v-row class="d-flex">
-      <v-col class="d-flex flex-column justify-center" cols="12" md="6">
-        <v-row class="d-flex" :class="displayStore.xs ? '' : displayStore.sm ? '' : 'ga-3'" dense>
-          <v-col class="d-flex align-center pa-0" cols="12" sm="6">
-            <v-select
-              v-model="stationStore.stationId"
-              item-title="name"
-              item-value="id"
-              :items="stationStore.stations"
-              label="Escolha a estação"
-              variant="underlined"
-              @update:model-value="stationStore.getStationLines"
-            />
-          </v-col>
+  <TitleSection title="Dashboard Geral" />
+  <v-row class="d-flex">
+    <v-col class="d-flex flex-column justify-center" cols="12" md="6">
+      <v-row
+        class="d-flex"
+        :class="displayStore.xs ? '' : displayStore.sm ? '' : 'ga-3'"
+        dense
+      >
+        <v-col class="d-flex align-center pa-0" cols="12" sm="6">
+          <v-select
+            v-model="stationStore.stationId"
+            item-title="name"
+            item-value="id"
+            :items="stationStore.stations"
+            label="Escolha a estação"
+            variant="underlined"
+            @update:model-value="stationStore.getStationLines"
+          />
+        </v-col>
 
-          <v-col
-            class="d-flex align-center pa-0"
-            :class="displayStore.xs ? 'justify-center' : displayStore.sm ? 'justify-center' : 'justify-start'"
-            cols="12"
-            sm="5"
-          >
-            <FindStationDialog />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12">
+        <v-col
+          class="d-flex align-center pa-0"
+          :class="
+            displayStore.xs
+              ? 'justify-center'
+              : displayStore.sm
+              ? 'justify-center'
+              : 'justify-start'
+          "
+          cols="12"
+          sm="5"
+        >
+          <FindStationDialog />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-card variant="tonal">
             <v-table
+              class="bg-transparent"
               v-if="stationStore.stationLines != null"
               fixed-header
             >
               <thead>
                 <tr>
-                  <th class="text-left">Nome</th>
-                  <th class="text-left">Próx. Parada</th>
-                  <th class="text-left">Destino Final</th>
+                  <th class="text-left bg-transparent">Nome</th>
+                  <th class="text-left bg-transparent">Próx. Parada</th>
+                  <th class="text-left bg-transparent">Destino Final</th>
                 </tr>
               </thead>
               <tbody>
@@ -58,14 +70,13 @@
                 </tr>
               </tbody>
             </v-table>
-          </v-col>
-        </v-row>
-      </v-col>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
 
-      <v-col cols="12" md="6">
-        <WarningsList
-          :items="warningStore.warnings"
-        />
-      </v-col>
-    </v-row>
+    <v-col cols="12" md="6">
+      <WarningsList :items="warningStore.warnings" />
+    </v-col>
+  </v-row>
 </template>
